d58f6edfd728fe9c2f0bce67e358ef667dbf1197
Thomas Ingram <code@ravinggenius.com>
2023-06-13 10:28:44 -0400

    Initialize database configuration

--- src/library/_/config.mjs
+++ src/library/_/config.mjs
@@ -5,10 +5,24 @@ export * from "./config-public.mjs";
 
 const env = createEnv({
 	runtimeEnvStrict: {
-		DATABASE_URL: process.env.DATABASE_URL
+		DATABASE_URL: process.env.DATABASE_URL,
+		NODE_ENV: process.env.NODE_ENV
 	},
-	server: { DATABASE_URL: z.string().url() }
+	server: {
+		DATABASE_URL: z.string().url(),
+		NODE_ENV: z.union([
+			z.literal("development"),
+			z.literal("production"),
+			z.literal("test")
+		])
+	}
 });
+
+export const isDevelopment = env.NODE_ENV === "development";
+
+export const isProduction = env.NODE_ENV === "production";
+
+export const isTest = env.NODE_ENV === "test";
 
 /**
  * Postgres database connection URL. Can point to a connection pool
---
+++ src/migrations/20230613135343892_create-extension-uuid.sql
@@ -0,0 +1,7 @@
+-- Up Migration
+
+CREATE EXTENSION "uuid-ossp";
+
+-- Down Migration
+
+DROP EXTENSION "uuid-ossp";

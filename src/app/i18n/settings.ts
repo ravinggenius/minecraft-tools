4a538b4944729690bec403b9a86e0bee4a0868fb
Thomas Ingram <code@ravinggenius.com>
2023-08-31 00:19:01 -0400

    Detect user locale

--- src/app/i18n/settings.ts
+++ src/app/i18n/settings.ts
@@ -3,3 +3,5 @@ export const FALLBACK_LOCALE = "en-US" as const;
 export const SUPPORTED_LOCALES = [FALLBACK_LOCALE] as const;
 
 export type SupportedLocale = (typeof SUPPORTED_LOCALES)[number];
+
+export const cookieName = "_locale";
---
+++ src/middleware.ts
@@ -0,0 +1,55 @@
+import acceptLanguage from "accept-language";
+import { NextRequest, NextResponse } from "next/server";
+
+import {
+	FALLBACK_LOCALE,
+	SUPPORTED_LOCALES,
+	cookieName
+} from "@/app/i18n/settings";
+
+acceptLanguage.languages([...SUPPORTED_LOCALES]);
+
+export const config = {
+	matcher: ["/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js).*)"]
+};
+
+export const middleware = (request: NextRequest) => {
+	let locale;
+
+	if (request.cookies.has(cookieName)) {
+		locale = acceptLanguage.get(request.cookies.get(cookieName)?.value);
+	}
+	if (!locale) {
+		locale = acceptLanguage.get(request.headers.get("Accept-Language"));
+	}
+	if (!locale) {
+		locale = FALLBACK_LOCALE;
+	}
+
+	if (
+		!SUPPORTED_LOCALES.some((loc) =>
+			request.nextUrl.pathname.startsWith(`/${loc}`)
+		) &&
+		!request.nextUrl.pathname.startsWith("/_next")
+	) {
+		return NextResponse.redirect(
+			new URL(`/${locale}${request.nextUrl.pathname}`, request.url)
+		);
+	}
+
+	if (request.headers.has("referer")) {
+		const refererUrl = new URL(request.headers.get("referer") as string);
+
+		const lngInReferer = SUPPORTED_LOCALES.find((l) =>
+			refererUrl.pathname.startsWith(`/${l}`)
+		);
+
+		const response = NextResponse.next();
+
+		if (lngInReferer) response.cookies.set(cookieName, lngInReferer);
+
+		return response;
+	}
+
+	return NextResponse.next();
+};

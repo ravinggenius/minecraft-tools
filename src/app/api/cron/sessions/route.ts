f6d51b9f0b09b6f5ed56316fae4e8f53d9c390a4
Thomas Ingram <code@ravinggenius.com>
2023-12-03 12:34:39 -0500

    Split cron job into individual jobs
    Jobs may need to be merged back together later

--- src/app/api/cron/sessions/route.ts
+++ src/app/api/cron/sessions/route.ts
@@ -1,11 +1,7 @@
-import * as passwordReset from "@/library/password-reset/model";
 import * as sessionModel from "@/library/session/model";
 
 export const GET = async (request: Request) => {
-	await Promise.allSettled([
-		passwordReset.clearExpired(),
-		sessionModel.clearExpired()
-	]);
+	await sessionModel.clearExpired();
 
 	return new Response(undefined, {
 		status: 204

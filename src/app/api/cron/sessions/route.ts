218a8d3abeeba715c950af95a98452c5c0fd3a67
Thomas Ingram <code@ravinggenius.com>
2024-01-24 00:34:23 -0500

    Ensure cron routes are run dynamically

--- src/app/api/cron/sessions/route.ts
+++ src/app/api/cron/sessions/route.ts
@@ -1,6 +1,10 @@
+import { unstable_noStore } from "next/cache";
+
 import * as sessionModel from "@/library/session/model";
 
 export const GET = async (request: Request) => {
+	unstable_noStore();
+
 	await sessionModel.clearExpired();
 
 	return new Response(undefined, {

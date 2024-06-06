ab0858dac16b9948bc5347653283f1ddb0f362b0
Thomas Ingram <code@ravinggenius.com>
2024-06-06 12:48:46 -0400

    Remove superfluous library

--- src/services/config-public-service.mjs
+++ src/services/config-public-service.mjs
@@ -1,28 +1,15 @@
-import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
-const env = createEnv({
-	runtimeEnv: {
-		NEXT_PUBLIC_DEBUG_I18N: process.env.NEXT_PUBLIC_DEBUG_I18N,
-		NEXT_PUBLIC_I18N_NAME: process.env.NEXT_PUBLIC_I18N_NAME,
-		NEXT_PUBLIC_PASSWORD_MIN_LENGTH:
-			process.env.NEXT_PUBLIC_PASSWORD_MIN_LENGTH
-	},
-	client: {
-		NEXT_PUBLIC_DEBUG_I18N: z
-			.string()
-			.refine((s) => s === "true" || s === "false")
-			.transform((s) => s === "true"),
-		NEXT_PUBLIC_I18N_NAME: z.string(),
-		NEXT_PUBLIC_PASSWORD_MIN_LENGTH: z
-			.string()
-			.transform((n) => Number.parseInt(n, 10))
-			.pipe(z.number().positive())
-	}
-});
+export const debugI18n = z
+	.string()
+	.refine((s) => s === "true" || s === "false")
+	.transform((s) => s === "true")
+	.parse(process.env.NEXT_PUBLIC_DEBUG_I18N);
 
-export const debugI18n = env.NEXT_PUBLIC_DEBUG_I18N;
+export const i18nName = z.string().parse(process.env.NEXT_PUBLIC_I18N_NAME);
 
-export const i18nName = env.NEXT_PUBLIC_I18N_NAME;
-
-export const passwordMinLength = env.NEXT_PUBLIC_PASSWORD_MIN_LENGTH;
+export const passwordMinLength = z
+	.string()
+	.transform((n) => Number.parseInt(n, 10))
+	.pipe(z.number().positive())
+	.parse(process.env.NEXT_PUBLIC_PASSWORD_MIN_LENGTH);
--- src/services/config-service.mjs
+++ src/services/config-service.mjs
@@ -1,117 +1,89 @@
-import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export * from "./config-public-service.mjs";
 
-const env = createEnv({
-	runtimeEnvStrict: {
-		DATABASE_URL: process.env.DATABASE_URL,
-		EMAIL_AUTH_USERNAME: process.env.EMAIL_AUTH_USERNAME,
-		EMAIL_AUTH_PASSWORD: process.env.EMAIL_AUTH_PASSWORD,
-		EMAIL_FROM_ADDRESS: process.env.EMAIL_FROM_ADDRESS,
-		EMAIL_HOST: process.env.EMAIL_HOST,
-		EMAIL_PORT: process.env.EMAIL_PORT,
-		EMAIL_SECURE: process.env.EMAIL_SECURE,
-		EMAIL_RESEND_EXPIRY_MINUTES: process.env.EMAIL_RESEND_EXPIRY_MINUTES,
-		EMAIL_VERIFICATION_EXPIRY_DAYS:
-			process.env.EMAIL_VERIFICATION_EXPIRY_DAYS,
-		ENCRYPTION_SECRET: process.env.ENCRYPTION_SECRET,
-		HOST_URL: process.env.HOST_URL,
-		NODE_ENV: process.env.NODE_ENV,
-		PASSWORD_SALT_ROUNDS: process.env.PASSWORD_SALT_ROUNDS,
-		SESSION_COOKIE_PATH: process.env.SESSION_COOKIE_PATH,
-		SESSION_COOKIE_SAME_SITE: process.env.SESSION_COOKIE_SAME_SITE,
-		SESSION_MAX_AGE_SECONDS: process.env.SESSION_MAX_AGE_SECONDS,
-		SESSION_NAME: process.env.SESSION_NAME,
-		SESSION_ASSISTANCE_PASSWORD_RESET_EXPIRY_MINUTES:
-			process.env.SESSION_ASSISTANCE_PASSWORD_RESET_EXPIRY_MINUTES
-	},
-	server: {
-		DATABASE_URL: z.string().url(),
-		EMAIL_AUTH_USERNAME: z.string(),
-		EMAIL_AUTH_PASSWORD: z.string(),
-		EMAIL_FROM_ADDRESS: z.string().email(),
-		EMAIL_HOST: z.string(),
-		EMAIL_PORT: z
-			.string()
-			.transform((n) => Number.parseInt(n, 10))
-			.pipe(z.number().min(1).max(65535)),
-		EMAIL_SECURE: z
-			.string()
-			.refine((s) => s === "true" || s === "false")
-			.transform((s) => s === "true"),
-		EMAIL_RESEND_EXPIRY_MINUTES: z
-			.string()
-			.transform((n) => Number.parseInt(n, 10))
-			.pipe(z.number().positive()),
-		EMAIL_VERIFICATION_EXPIRY_DAYS: z
-			.string()
-			.transform((n) => Number.parseInt(n, 10)),
-		ENCRYPTION_SECRET: z.string().min(32),
-		HOST_URL: z.string().url(),
-		NODE_ENV: z.enum(["development", "production", "test"]),
-		PASSWORD_SALT_ROUNDS: z
-			.string()
-			.transform((n) => Number.parseInt(n, 10))
-			.pipe(z.number().positive()),
-		SESSION_COOKIE_PATH: z.string().refine((path) => path.startsWith("/")),
-		SESSION_COOKIE_SAME_SITE: z.enum(["lax", "none", "strict"]),
-		SESSION_MAX_AGE_SECONDS: z
-			.string()
-			.transform((n) => Number.parseInt(n, 10))
-			.pipe(z.number().positive()),
-		SESSION_NAME: z.string(),
-		SESSION_ASSISTANCE_PASSWORD_RESET_EXPIRY_MINUTES: z
-			.string()
-			.transform((n) => Number.parseInt(n, 10))
-			.pipe(z.number().positive())
-	}
-});
+export const isDevelopment = process.env.NODE_ENV === "development";
 
-export const isDevelopment = env.NODE_ENV === "development";
+export const isProduction = process.env.NODE_ENV === "production";
 
-export const isProduction = env.NODE_ENV === "production";
-
-export const isTest = env.NODE_ENV === "test";
+export const isTest = process.env.NODE_ENV === "test";
 
 /**
  * Postgres database connection URL. Can point to a connection pool
  */
-export const databaseUrl = env.DATABASE_URL;
+export const databaseUrl = z.string().url().parse(process.env.DATABASE_URL);
 
 export const email = {
 	auth: {
-		username: env.EMAIL_AUTH_USERNAME,
-		password: env.EMAIL_AUTH_PASSWORD
+		username: z.string().parse(process.env.EMAIL_AUTH_USERNAME),
+		password: z.string().parse(process.env.EMAIL_AUTH_PASSWORD)
 	},
 	from: {
-		address: env.EMAIL_FROM_ADDRESS
+		address: z.string().email().parse(process.env.EMAIL_FROM_ADDRESS)
 	},
-	host: env.EMAIL_HOST,
-	port: env.EMAIL_PORT,
-	secure: env.EMAIL_SECURE
+	host: z.string().parse(process.env.EMAIL_HOST),
+	port: z
+		.string()
+		.transform((n) => Number.parseInt(n, 10))
+		.pipe(z.number().min(1).max(65535))
+		.parse(process.env.EMAIL_PORT),
+	secure: z
+		.string()
+		.refine((s) => s === "true" || s === "false")
+		.transform((s) => s === "true")
+		.parse(process.env.EMAIL_SECURE)
 };
 
-export const emailResendExpiryMinutes = env.EMAIL_RESEND_EXPIRY_MINUTES;
+export const emailResendExpiryMinutes = z
+	.string()
+	.transform((n) => Number.parseInt(n, 10))
+	.pipe(z.number().positive())
+	.parse(process.env.EMAIL_RESEND_EXPIRY_MINUTES);
 
-export const emailVerificationExpiryDays = env.EMAIL_VERIFICATION_EXPIRY_DAYS;
+export const emailVerificationExpiryDays = z
+	.string()
+	.transform((n) => Number.parseInt(n, 10))
+	.parse(process.env.EMAIL_VERIFICATION_EXPIRY_DAYS);
 
-export const encryptionSecret = env.ENCRYPTION_SECRET;
+export const encryptionSecret = z
+	.string()
+	.min(32)
+	.parse(process.env.ENCRYPTION_SECRET);
 
-export const hostUrl = env.HOST_URL;
+export const hostUrl = z.string().url().parse(process.env.HOST_URL);
 
-export const passwordSaltRounds = env.PASSWORD_SALT_ROUNDS;
+export const nodeEnv = z
+	.enum(["development", "production", "test"])
+	.parse(process.env.NODE_ENV);
+
+export const passwordSaltRounds = z
+	.string()
+	.transform((n) => Number.parseInt(n, 10))
+	.pipe(z.number().positive())
+	.parse(process.env.PASSWORD_SALT_ROUNDS);
 
 export const sessionCookieOptions = {
 	httpOnly: true,
-	path: env.SESSION_COOKIE_PATH,
-	sameSite: env.SESSION_COOKIE_SAME_SITE,
+	path: z
+		.string()
+		.refine((path) => path.startsWith("/"))
+		.parse(process.env.SESSION_COOKIE_PATH),
+	sameSite: z
+		.enum(["lax", "none", "strict"])
+		.parse(process.env.SESSION_COOKIE_SAME_SITE),
 	secure: isProduction
 };
 
-export const sessionMaxAgeSeconds = env.SESSION_MAX_AGE_SECONDS;
+export const sessionMaxAgeSeconds = z
+	.string()
+	.transform((n) => Number.parseInt(n, 10))
+	.pipe(z.number().positive())
+	.parse(process.env.SESSION_MAX_AGE_SECONDS);
 
-export const sessionName = env.SESSION_NAME;
+export const sessionName = z.string().parse(process.env.SESSION_NAME);
 
-export const sessionAssistancePasswordResetExpiryMinutes =
-	env.SESSION_ASSISTANCE_PASSWORD_RESET_EXPIRY_MINUTES;
+export const sessionAssistancePasswordResetExpiryMinutes = z
+	.string()
+	.transform((n) => Number.parseInt(n, 10))
+	.pipe(z.number().positive())
+	.parse(process.env.SESSION_ASSISTANCE_PASSWORD_RESET_EXPIRY_MINUTES);

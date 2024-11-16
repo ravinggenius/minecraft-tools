85cd61d1d7ec15ab9958c3ea3337d6c4abd689b1
Thomas Ingram <code@ravinggenius.com>
2024-11-15 19:17:33 -0500

    Stop assuming Next will cache dynamic routes

--- src/app/api/cron/sessions/route.ts
+++ src/app/api/cron/sessions/route.ts
@@ -1,11 +1,7 @@
-import { unstable_noStore } from "next/cache";
-
 import * as sessionModel from "@/domains/session/model";
 import { RouteMethod } from "@/library/route-meta.schema";
 
 export const GET: RouteMethod = async (request, { params }) => {
-	unstable_noStore();
-
 	await sessionModel.clearExpired();
 
 	return new Response(undefined, {

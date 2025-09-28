7aaf8a0ff9b49766906d2cf97c766d9122659501
Thomas Ingram <code@ravinggenius.com>
2025-09-28 14:29:44 -0400

    Simplify fixture loading and be more explicit about what is loaded

--- src/scripts/import-releases.ts
+++ src/scripts/import-releases.ts
@@ -2,8 +2,7 @@ import * as releaseModel from "@/domains/release/model";
 import { IMPORT_RELEASES } from "@/domains/release/schema";
 import rawImportReleases from "@/services/datastore-service/fixtures/releases.json";
 
-export const execute = () =>
-	IMPORT_RELEASES.parse(rawImportReleases).reduce(
-		(memo, release) => memo.then(() => releaseModel.doImport(release)),
-		Promise.resolve([]) as ReturnType<typeof releaseModel.doImport>
-	);
+IMPORT_RELEASES.parse(rawImportReleases).reduce(
+	(memo, release) => memo.then(() => releaseModel.doImport(release)),
+	Promise.resolve([]) as ReturnType<typeof releaseModel.doImport>
+);
